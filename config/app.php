ae51f3e1c431039ad85aaae9f87ac9e162d2d9c4
elangptra <rvnlz17@gmail.com>
2023-05-09 18:19:38 +0700

    Update Praktikum 3 - Reporting

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -193,5 +194,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // ...
     ])->toArray(),
+
+    'PDF' => Barryvdh\DomPDF\Facade::class,
 
 ];
